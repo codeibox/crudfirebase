bbf658206fbd6955a691a81a997a6678e1d15243
José Augusto <60748380+codeibox@users.noreply.github.com>
2024-03-26 09:07:47 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import firebase from 'firebase'
 // Sua key do firebase adicione ela aqui em baixo
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCeBfDBG_cS8cOaLeJO0-79sc4GtqQr_YY",
+  authDomain: "appdasos-56cf2.firebaseapp.com",
+  databaseURL: "https://appdasos-56cf2-default-rtdb.firebaseio.com",
+  projectId: "appdasos-56cf2",
+  storageBucket: "appdasos-56cf2.appspot.com",
+  messagingSenderId: "497041951362",
+  appId: "1:497041951362:web:99205a9a1760729934029b"
 };
 
 const fireDb = firebase.initializeApp(firebaseConfig);
